baaa624c878b718d58a861fecd43a235682ffba3
Sarah Cameron Pesant <cp.sarah@gmail.com>
2022-05-16 14:22:15 -0400

    Adding unit test

--- sarah_project/lib.py
+++ sarah_project/lib.py
@@ -1,6 +1,5 @@
 def try_me():
-    name = input('Enter your name:')
-    return print(f'Hello {name}, have a nice day!')
+    return print(f'Hello you, have a nice day!')
 
 if __name__ == "__main__":
     try_me()
